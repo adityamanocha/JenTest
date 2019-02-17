c3eec28d708154e3040eb42d257927fb30829d98
Aditya M <aditya.manocha@humanebits.com>
2019-02-17 20:37:02 +0530

    t

---
+++ ex1.py
@@ -0,0 +1,10 @@
+print ("Hello World!")
+print ("Hello Again")
+print ("Believe! Believe! Believe!")
+print ("This is 'not' boring at all, this is fun")
+print ("I'd love to clear this")
+print ('I "said" I will learn for sure.')
+print ("\n")
+print ("New Line")
+#stuff comment
+print ("#stuff")
---
+++ test.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Aditya.manocha\JenTesting\
+python ex1.py
